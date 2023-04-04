bf1124180fb4d78b4b92851bbd295637d54ec0ce
GuilhermeRodrigues22 <guicordeiro556@gmail.com>
2023-04-04 17:05:43 -0300

    funções matemática

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-function'`, () => {
+  it(`should have as title 'angular-calc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-function');
+    expect(app.title).toEqual('angular-calc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-function app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-calc app is running!');
   });
 });
